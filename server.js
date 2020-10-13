df5cd15ec9d4b9b3a7f0985e9e800f99c1dbc6a3
mauriciom75 <mauriciom7575@gmail.com>
2020-10-13 16:54:42 -0300

    Update server.js

--- server.js
+++ server.js
@@ -3,12 +3,24 @@
 const spawn = require('child_process').spawn;
 const exec = require('child_process').execSync;
 const open = require('open');
+const fs = require('fs'); 
 
 var args = process.argv;
 var settings = process.env.NR_SETTINGS_FILE || "/nr_conf/settings.js"
 if (args.find(a => a === '-f')) { const npm_install = exec('npm install -f'); }
 
-if (settings) { spawn('node', ['node_modules/node-red/red.js', '-s', settings, '-userDir', '.'], {stdio:'inherit'}); }
-else { spawn('node', ['node_modules/node-red/red.js', '-userDir', '.'], {stdio:'inherit'}); }
 
-(async () => { await open('http://localhost:'+(port || 1880)); })();
+fs.copyFile("/nr_conf/flows.json", "./flows.json", (err) => { 
+  if (err) { 
+    console.log("warning: Archivo no encontrado:", err); 
+  } 
+  else { 
+  
+      if (settings) { spawn('node', ['node_modules/node-red/red.js', '-s', settings, '-userDir', '.'], {stdio:'inherit'}); }
+      else { spawn('node', ['node_modules/node-red/red.js', '-userDir', '.'], {stdio:'inherit'}); }
+ 
+  } 
+}); 
+
+
+(async () => { await open('http://localhost:'+ '1880'); })();
