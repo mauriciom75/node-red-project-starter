2cdf09ee91d8fbaa7751ea288beb0b907d40b373
mauriciom75 <mauriciom7575@gmail.com>
2020-10-13 13:13:09 -0300

    Update server.js

--- server.js
+++ server.js
@@ -5,8 +5,7 @@ const exec = require('child_process').execSync;
 const open = require('open');
 
 var args = process.argv;
-var settings = parseInt(args[2]);
-
+var settings = process.env.NR_SETTINGS_FILE || "/nr_conf/settings.js"
 if (args.find(a => a === '-f')) { const npm_install = exec('npm install -f'); }
 
 if (settings) { spawn('node', ['node_modules/node-red/red.js', '-s', settings, '-userDir', '.'], {stdio:'inherit'}); }
